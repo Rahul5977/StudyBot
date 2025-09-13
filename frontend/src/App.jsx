b473cf11f2ab4771b3b8d7b1479a3d434bd206bb
Rahul Raj <rahul.raj9237@gmail.com>
2025-09-13 19:45:00 +0530

    feat: add main React App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,13 @@
-// App.jsx
+import React from "react";
+import Home from "./pages/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
